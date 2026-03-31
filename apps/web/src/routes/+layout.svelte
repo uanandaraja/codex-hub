75854266e696966c76c8beddaf7138fe04386e9e
nizzy <uanandaraja@gmail.com>
2026-03-31 21:41:15 +0700

    Use local web fonts

--- apps/web/src/routes/+layout.svelte
+++ apps/web/src/routes/+layout.svelte
@@ -1,10 +1,5 @@
 <script lang="ts">
 	import '../app.css';
-	import '@fontsource/space-grotesk/400.css';
-	import '@fontsource/space-grotesk/500.css';
-	import '@fontsource/space-grotesk/700.css';
-	import '@fontsource/ibm-plex-mono/400.css';
-	import '@fontsource/ibm-plex-mono/500.css';
 	import favicon from '$lib/assets/favicon.svg';
 
 	let { children } = $props();
